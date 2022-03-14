ce4475031f24206a80d48b5a3c6e3b6ccb07a8ac
yuma <yuma@example.com>
2022-03-14 12:23:08 +0900

    addColumnコンポーネント作成完了#11

--- src/component/Dashboard.tsx
+++ src/component/Dashboard.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
@@ -13,6 +13,8 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -112,6 +114,14 @@ const Dashboard = () => {
   const SignOut = () => {
     signOut(auth);
   };
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const expansion = Boolean(anchorEl);
+  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -166,13 +176,34 @@ const Dashboard = () => {
               displayName={LoginUser.displayName}
             />
 
-            <ListItemButton>
+            <ListItemButton
+              aria-label="more"
+              id="long-button"
+              aria-controls={expansion ? 'long-menu' : undefined}
+              aria-expanded={expansion ? 'true' : undefined}
+              aria-haspopup="true"
+              onClick={handleClick}
+            >
               <ListItemIcon>
                 <AddIcon />
               </ListItemIcon>
               <ListItemText primary="Add Column" />
             </ListItemButton>
             <Divider />
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={expansion}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem onClick={handleClose}>Home</MenuItem>
+              <MenuItem onClick={handleClose}>User</MenuItem>
+              <MenuItem onClick={handleClose}>Chart</MenuItem>
+              <MenuItem onClick={handleClose}>Price</MenuItem>
+            </Menu>
           </List>
         ) : (
           <List>
